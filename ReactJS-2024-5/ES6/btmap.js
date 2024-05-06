853ed50dbb8122bd04d22a76c1f1357b4d3fbd21
tranlinhcntt <tranlinhcntt@gmail.com>
2024-05-07 00:34:41 +0900

    map eps6

---
+++ ReactJS-2024-5/ES6/btmap.js
@@ -0,0 +1,11 @@
+const birds = [
+    {"ID": "DV8", "Name": "Eurasian Collared-Dove", "Type": "Dove" },
+    {"ID": "HK12", "Name": "Bald Eagle", "Type": "Hawk" },
+    {"ID": "HK6", "Name": "Cooper's Hawk", "Type": "Hawk" },
+    {"ID": "SP11", "Name": "Bell's Sparrow", "Type": "Sparrow" },
+    {"ID": "DV2", "Name": "Mourning Dove", "Type": "Dove" }
+];
+const birdIDs = birds.map(bird => bird.ID);
+console.log(birdIDs);
+
+
